77615dabda4bc91882d37b59db9b92432ec43fe6
funnyman7777 <computernet2000@gmail.com>
2020-12-04 04:22:01 +0200

    initial commit

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 import requests
 import json
+import re
 
-match = "db.t3"
 url = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonRDS/20201128151805/ap-south-1/index.json"
 
 response = requests.get(url)
@@ -10,12 +10,11 @@ result = json.loads(response.text)
 for nn in result["terms"]["OnDemand"]:
     # print(nn)
     for ff in result["terms"]["OnDemand"][nn]:
-        # print(ff)
+
         for mm in result["terms"]["OnDemand"][nn][ff]["priceDimensions"]:
-            print("")
-            for ww in result["terms"]["OnDemand"][nn][ff]["priceDimensions"][mm]["description"]:
-                # print(result["terms"]["OnDemand"][nn][ff]["priceDimensions"][mm]["pricePerUnit"]["USD"])
-               # if match in ww:
-                    print(ww, end = '')
-               # else:
-                   # print("not found")
+
+            for ww in result["terms"]["OnDemand"][nn][ff]["priceDimensions"][mm]["description"].split('\n'):
+
+                if re.search("db.t3", ww.strip()):
+                    print(ww.strip())
+                    print("PRICE ", result["terms"]["OnDemand"][nn][ff]["priceDimensions"][mm]["pricePerUnit"]["USD"].split('\n'), "USD")
